bf39a1cf9df45c850aab365628333b428c919cee
chelsea-zhou <zcshu126@163.com>
2024-12-03 22:59:51 -0500

    Implement createRecording API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from 'express';
+import express, {Request,  Response } from "express";
 import dotenv from "dotenv";
+import { createRecording } from './controllers';
 
 // Load .env file contents into process.env
 dotenv.config();
@@ -19,8 +20,24 @@ app.get('/recordings/:user_id/:recording_id', (req: Request, res: Response) => {
   res.send('get one recording');
 });
 
-app.post('/recordings/:user_id/', (req: Request, res: Response) => {
-  res.send('create recording');
+app.post('/recordings/:user_id/', async (req: Request, res: Response): Promise<any> => {
+  const recordingUrl = req.body.recording_url;
+  const userId = req.params.user_id;
+  if (!recordingUrl || !userId) {
+    return res.status(400).json({ error: 'Recording URL and User Id can\'t be empty' });
+  };
+  const createRequest = {
+    recordingUrl: req.body.recording_url,
+    userId: req.params.user_id
+  }
+  try {
+    const response = await createRecording(createRequest);
+    return res.json(response);
+  } catch (error) {
+    console.error(`Error while creating record:`, error);
+    return res.status(500).json({ error: "Failed to transcribe recording" });
+  }
+
 });
 
 app.delete('/recordings/:user_id/:recording_id', (req: Request, res: Response) => {
