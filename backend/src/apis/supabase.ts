0b96d7b07ffd3a197c6cf20e9c6d90715ac118d8
chelsea-zhou <zcshu126@163.com>
2024-12-05 13:22:24 -0500

    Implement APIs + db operations

--- backend/src/apis/supabase.ts
+++ backend/src/apis/supabase.ts
@@ -1,14 +1,19 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from "dotenv";
+import { BUCKET_NAME } from '../constant';
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_API_KEY;
+const supabase = createClient(supabaseUrl!, supabaseKey!);
 
 // created sign url for frontend to upload audio file
 export const generatePresignedUrl = async (userId: string, filename: string): Promise<string> => {
-    const supabaseUrl = process.env.SUPABASE_URL;
-    const supabaseKey = process.env.SUPABASE_API_KEY;
-    const supabase = createClient(supabaseUrl!, supabaseKey!);
-    const bucketName = "recordings";
+
     const { data, error } = await supabase
         .storage
-        .from(bucketName)
+        .from(BUCKET_NAME)
         .createSignedUploadUrl(`${userId}/${filename}`);
 
     if (error) {
@@ -17,4 +22,34 @@ export const generatePresignedUrl = async (userId: string, filename: string): Pr
     }
     const { signedUrl, token } = data;
     return signedUrl;
+}
+
+// Function to upload a file buffer
+export async function uploadBuffer(bucketName: string=BUCKET_NAME, filePath: string, buffer: Buffer, mimeType: string="audio/x-m4a") {
+    const { data, error } = await supabase.storage
+      .from(bucketName)
+      .upload(filePath, buffer, {
+        contentType: mimeType,
+      });
+  
+    if (error) {
+      console.error('Error uploading buffer:', error);
+      throw new Error('Error uploading recording to storage');
+    }
+  
+    console.log('Buffer uploaded successfully:', data);
+    return data;
+  }
+
+export async function downloadFile(bucketName=BUCKET_NAME, filePath: string ) {
+    const { data, error } = await supabase.storage
+      .from(bucketName)
+      .download(filePath);
+
+    if (error) {
+      console.error('Error downloading blob:', error);
+      throw new Error('Error downloading recording from storage');
+    }
+    console.log('blob downloaded successfully:', data);
+    return data;
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request,  Response } from "express";
 import dotenv from "dotenv";
-import { createRecording } from './controllers';
+import { createRecording, deleteRecording, getRecording, listRecordings, updateRecording } from './controllers';
 import { generatePresignedUrl } from "./apis/supabase";
 
 dotenv.config();
@@ -10,8 +10,15 @@ const PORT = 3000;
 app.use(express.json());
 
 // list recordings
-app.get('/recordings', (req: Request, res: Response) => {
-  res.send('get list of recordings');
+app.get('/recordings/:user_id', async (req: Request, res: Response): Promise<any> => {
+  const {user_id} = req.params;
+  try {
+    const recordingsInfo = await listRecordings({userId: user_id});
+    return res.json(recordingsInfo);
+  } catch(error) {
+    console.error(`Error while retrieving conversations:`, error);
+    return res.status(500).json({ error: "Failed to retrieve conversations" });
+  }
 });
 
 // generate presigned url for audio upload
@@ -30,10 +37,29 @@ app.post('/recordings/:user_id/upload', async (req: Request, res: Response): Pro
   }
 });
 
-app.get('/recordings/:user_id/:recording_id', (req: Request, res: Response) => {
-  res.send('get one recording');
+// get single recording
+app.get('/recordings/:user_id/:recording_id', async (req: Request, res: Response): Promise<any> => {
+  const {user_id, recording_id} = req.params;
+  if (!user_id || !recording_id) {
+    return res.status(400).json({ error: 'Recording Id and User Id can\'t be empty' });
+  }
+  const getRequest = {
+    userId: user_id,
+    recordingId: recording_id
+  }
+  try {
+    const recording = await getRecording(getRequest);
+    if (!recording) {
+      return res.status(404).json({ error: `Recording ${recording_id} not found` });
+    }
+    return res.json(recording);
+  } catch (error) {
+    console.error(`Error while getting recording:`, error);
+    return res.status(500).json({ error: "Failed to get recording" });
+  }
 });
 
+// create recording
 app.post('/recordings/:user_id', async (req: Request, res: Response): Promise<any> => {
   const recordingUrl = req.body.recording_url;
   const userId = req.params.user_id;
@@ -53,8 +79,57 @@ app.post('/recordings/:user_id', async (req: Request, res: Response): Promise<an
   }
 });
 
-app.delete('/recordings/:user_id/:recording_id', (req: Request, res: Response) => {
-  res.send('delete one recording');
+// delete recording
+app.delete('/recordings/:user_id/:recording_id', async (req: Request, res: Response): Promise<any> => {
+  const {user_id, recording_id} = req.params;
+  if (!user_id || !recording_id) {
+    return res.status(400).json({ error: 'Recording Id and User Id can\'t be empty' });
+  }
+
+  try {
+    const result = await deleteRecording({
+      userId: user_id,
+      recordingId: recording_id
+    });
+    return res.status(200).json({message: "successfully deleted conversation"});
+  } catch (error: any) {
+    if (error.code === 'P2025') {
+      console.error('Error: The record you tried to delete does not exist.');
+      return res.status(400).json({ error: "The conversation you tried to delete does not exist" });
+    } else {
+      console.error(`Error while deleting recording:`, error);
+      return res.status(500).json({ error: "Failed to delete recording" });
+    }
+  }
+});
+
+app.put('/recordings/:user_id/:recording_id', async (req: Request, res: Response): Promise<any> => {
+  const {user_id, recording_id} = req.params;
+  const {title, transcript} = req.body;
+  if (!user_id || !recording_id) {
+    return res.status(400).json({ error: 'Recording Id and User Id can\'t be empty' });
+  }
+  if (!title || !transcript) {
+    return res.status(400).json({ error: 'Title and transcript can\'t be empty' });
+  }
+
+  try {
+    const result = await updateRecording({
+      userId: user_id,
+      recordingId: recording_id,
+      title,
+      transcript
+    });
+    return res.status(200).json({message: "successfully updated conversation"});
+  } catch (error: any) {
+    if (error.code === 'P2025') {
+      console.error('Error: The record you tried to update does not exist.');
+      return res.status(400).json({ error: "The conversation you tried to update does not exist" });
+    } else {
+      console.error(`Error while updating recording:`, error);
+      return res.status(500).json({ error: "Failed to update recording" });
+    }
+  }
 });
 
 // Start the server
