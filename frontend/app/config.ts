27858f52fd21d873cd0a05a1cd52e8a1a752a093
mahlen <malink027@gmail.com>
2024-12-06 21:56:57 -0500

    url updated

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,5 +1,5 @@
 export const ENV = {
     dev: 'http://localhost:3000',
     prod:  'https://conversechronicle-production.up.railway.app',
-    test: 'peaceful-recreation-malindev.up.railway.app'
+    test: 'https://peaceful-recreation-malindev.up.railway.app'
   }
